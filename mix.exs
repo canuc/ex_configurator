4ce4873f539d5085db717b2f056dc92205e30d29
Julian Haldenby <j.haldenby@gmail.com>
2022-09-03 03:25:52 -0400

    use 20.04

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExConfigurator.MixProject do
     [
       app: :ex_configurator,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env())
