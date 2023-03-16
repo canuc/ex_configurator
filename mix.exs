5ab19c614eeee97bfd854f212a6754a475517a19
Julian Haldenby <j.haldenby@gmail.com>
2023-03-16 17:36:37 -0400

    Publish v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,11 +4,26 @@ defmodule ExConfigurator.MixProject do
   def project do
     [
       app: :ex_configurator,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      source_url: "https://github.com/canuc/ex_configurator",
+      homepage_url: "https://github.com/canuc/ex_configurator",
+      docs: [
+        # The main page in the docs
+        main: "ExConfigurator",
+        logo: "priv/ex_configurator.png",
+        extras: ["README.md"]
+      ],
+      package: package(),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ],
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -22,8 +37,21 @@ defmodule ExConfigurator.MixProject do
     ]
   end
 
+  def package() do
+    [
+      links: %{"GitHub" => "https://github.com/canuc/ex_configurator"},
+      description: "Configuration tool.",
+      licenses: ["Apache 2.0"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.5.7", only: :test, runtime: false}
+    ]
   end
 end
